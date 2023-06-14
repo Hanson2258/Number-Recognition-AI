dd71d2666a2466718e37c7d73f96eb259e9d9d06
Hanson Chan <47832494+Hanson2258@users.noreply.github.com>
2023-06-13 18:50:54 -0700

    Retrained network, added comments
    1. Added comments to explain code segments.
    2. Retrained network, accuracy and loss numbers improved, but in testing, only got 14/19 sample digits correct.
    
    Need to rewrite network.

--- main.py
+++ main.py
@@ -10,31 +10,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
+# # Get data set
 # mnist = tf.keras.datasets.mnist
 # (x_train, y_train), (x_test, y_test) = mnist.load_data()
 #
+# # Normalize training data
 # x_train = tf.keras.utils.normalize(x_train, axis=1)
 # x_test = tf.keras.utils.normalize(x_test, axis=1)
-
+#
+# # Create Neural Network
 # model = tf.keras.models.Sequential()
 # model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
 # model.add(tf.keras.layers.Dense(128, activation='relu'))
 # model.add(tf.keras.layers.Dense(128, activation='relu'))
 # model.add(tf.keras.layers.Dense(10, activation='softmax'))
 #
+# # Run Neural Network
 # model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+# model.fit(x_train, y_train, epochs=100)
 #
-# model.fit(x_train, y_train, epochs=3)
-#
+# # Save Model
 # model.save('handwritten.model')
 
+# Load model
 model = tf.keras.models.load_model('handwritten.model')
 
+# # Evaluate model loss and accuracy (loss should be as low as possible, accuracy as high as possible)
 # loss, accuracy = model.evaluate(x_test, y_test)
-#
 # print(loss)
 # print(accuracy)
 
+
+# Test Model with sample data
 image_number = 1
 
 while os.path.isfile(f"sampleDigits/digit{image_number}.png"):
